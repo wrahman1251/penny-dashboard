5faf7bebf190ae285773fd0aeb730c6bda4e0503
Harrison Zhao <roarofpie@gmail.com>
2014-11-15 16:46:02 -0500

    added imp on/off

--- config/settings/secrets.js
+++ config/settings/secrets.js
@@ -1,5 +1,6 @@
 module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'Custom session secret',
   host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  baseUrl: 'https://agent.electricimp.com/yGnsf8EB3BJ2?led_'
 };
