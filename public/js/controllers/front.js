f6f43d7142416e8ba4669a70801c0cc1a6527b95
taylor <taylor@splashthat.com>
2014-11-15 21:39:12 -0500

    about tab, sliding screens

--- public/js/controllers/front.js
+++ public/js/controllers/front.js
@@ -3,11 +3,7 @@
 var front = angular.module('controllers.front', []);
 
 function Front($scope) {
-  console.log('hello world');
-  $scope.goAway = false;
-  $scope.awayToggle = function() {
- 	$scope.goAway = true;
-  }
+
 }
 
 front.controller('FrontCtrl', ['$scope', Front]);
