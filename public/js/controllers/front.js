2579fa0d4d903eb1df8777930832aa1edc073c7d
taylor <taylor@splashthat.com>
2014-11-16 03:45:17 -0500

    grab user input, convert all spaces to + signs

--- public/js/controllers/front.js
+++ public/js/controllers/front.js
@@ -3,7 +3,18 @@
 var front = angular.module('controllers.front', []);
 
 function Front($scope) {
-
+	$scope.address;
+	$scope.red = false;
+	$scope.goAway = false;
+	$scope.grabDestination = function() {
+		if ($scope.address != undefined) {
+			$scope.output = $scope.address.replace(' ','+');
+			alert($scope.output)
+			$scope.goAway = true;
+		} else {
+			$scope.red = true;
+		}
+	}
 }
 
 front.controller('FrontCtrl', ['$scope', Front]);
